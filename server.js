14bb4d9938d96710cf19d425eaaf1536412b5f44
ben-manning <bmanning@me.com>
2023-05-15 16:02:18 -0400

    add full CRUD cat routes

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
-app.listen(3000, () => {
+// ROUTES
+// GET all cats
+app.get('/cats', (req, res) => {
+  res.send('This is the GET route for all cats!');
+});
+
+// POST a new cat
+app.post('/cats', (req, res) => {
+  res.send('This is the POST route for a new cat!');
+});
+
+// GET a single cat
+app.get('/cats/:id', (req, res) => {
+  res.send(`This is the GET route for a single cat found at ${ req.params.id }`);
+});
+
+// PUT to update a cat
+app.put('/cats/:id', (req, res) => {
+  res.send(`This is the PUT route for a cat: ${req.params.id}`);
+});
+
+// DELETE to destroy a cat
+app.delete('/cats/:id', (req, res) => {
+  res.send(`This is the DELETE route for a cat:  Bye bye ${req.params.id}`);
+});
+
+app.listen(port, () => {
   console.log('I am listening at port 3000');
 });
